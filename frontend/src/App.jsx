54b694423081988c00aba0c08bd9af6dc358585b
Isaac Van Meter <isaacvanmeter@ibm.com>
2026-08-04 12:30:31 -0400

    feat: add login view and redirect root to login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './utils/ThemeContext';
-import OrdersView   from './pages/OrdersView';
-import ProductsView from './pages/ProductsView';
+import LoginView     from './pages/LoginView';
+import OrdersView    from './pages/OrdersView';
+import ProductsView  from './pages/ProductsView';
 import CustomersView from './pages/CustomersView';
 import './App.css';
 
@@ -11,11 +12,12 @@ export default function App() {
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/"          element={<Navigate to="/orders" replace />} />
+          <Route path="/"          element={<Navigate to="/login" replace />} />
+          <Route path="/login"     element={<LoginView />} />
           <Route path="/orders"    element={<OrdersView />} />
           <Route path="/products"  element={<ProductsView />} />
           <Route path="/customers" element={<CustomersView />} />
-          <Route path="*"          element={<Navigate to="/orders" replace />} />
+          <Route path="*"          element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
